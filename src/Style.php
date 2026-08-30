2f095e544ed7e00cb7edd34d4876b4b1a4868ea2
GlebPerfiliev <gleb.perfiliev@gmail.com>
2026-08-30 14:03:48 +0400

    css optimized and responsive separated

--- src/Style.php
+++ src/Style.php
@@ -3,19 +3,61 @@
 namespace App;
 
 use ScssPhp\ScssPhp\Compiler;
+use ScssPhp\ScssPhp\OutputStyle;
 
 class Style 
 {
+    const SCSS_PATH = '/assets/css/';
+
+    const SCSS_FILES = ['style', 'responsive'];
+
+    const CSS_PATH = '/public/css/';
+
+    const SCSS_EXT = '.scss';
+
+    const CSS_EXT = '.css';
+
     public static function init()
     {
         $appRoot = dirname(__DIR__);
+        $toCompile = [];
 
+        foreach (self::SCSS_FILES as $file) {
+            $filepath = $appRoot . self::SCSS_PATH . $file . self::SCSS_EXT;
+            $cssFilepath = $appRoot . self::CSS_PATH . $file . self::CSS_EXT;
+
+            if (
+                is_file($filepath) 
+                && (
+                    !is_file($cssFilepath)
+                    || filemtime($cssFilepath) < filemtime($filepath)
+                )
+            ) {
+                $toCompile[] = [$filepath, $cssFilepath];
+            }
+        }
+
+        if (!empty($toCompile)) {
+            self::compile($toCompile);
+        }
+    }
+
+    private static function compile($list)
+    {
+        $appRoot = dirname(__DIR__);
         $compiler = new Compiler();
-        $compiler->setImportPaths($appRoot . '/assets/css/');
+        $compiler->setImportPaths($appRoot . self::SCSS_PATH);
 
-        $scssCode = file_get_contents($appRoot . '/assets/css/style.scss');
-        $cssOutput = $compiler->compileString($scssCode)->getCss();
+        if (filter_var(getenv('CSS_MINIFY'), FILTER_VALIDATE_BOOLEAN)) {
+            $compiler->setOutputStyle(OutputStyle::COMPRESSED);
+        }
 
-        file_put_contents($appRoot . '/public/css/style.css', $cssOutput);
+        foreach ($list as $data) {
+            list($scssFilepath, $cssFilepath) = $data;
+            $scssCode = file_get_contents($scssFilepath);
+            $cssOutput = $compiler->compileString($scssCode)->getCss();
+
+            file_put_contents($cssFilepath, $cssOutput, LOCK_EX);
+        }
     }
 }
--- templates/components/head_styles.tpl
+++ templates/components/head_styles.tpl
@@ -1,1 +1,2 @@
 <link rel='stylesheet' href='/css/style.css' type='text/css' media='all' />
+<link rel='stylesheet' href='/css/responsive.css' type='text/css' media='all' />
